9c07f55c8404bc5023b3c6aac74f65aa92d39f6d
MakeDesire <ferrari219@nate.com>
2018-11-07 17:04:25 +0900

    classnames, include-media add

--- src/Components/SassComp.js
+++ src/Components/SassComp.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
 import styles from './SassComp.module.scss';
+
+const cx = classNames.bind(styles);
 
 class SassComp extends Component {
     render() {
         return (
-            <div className={styles.box}>
+            <div className={cx('box','b')}>
               SassComp  
             </div>
         );
